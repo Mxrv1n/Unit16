0111411a80e19b8be666de42443f8bd39b9f7335
Marvin P <MarvinPerez1503@outlook.com>
2026-05-05 10:10:33 -0400

    highs and lows

--- sitka_highs.py
+++ sitka_highs.py
@@ -4,27 +4,29 @@ from datetime import datetime
 
 import matplotlib.pyplot as plt
 
-path = Path("sitka_weather_07-2021_simple.csv")
+path = Path("sitka_weather_2021_simple.csv")
 lines = path.read_text(encoding="utf-8").splitlines()
 
 reader = csv.reader(lines)
 header_row = next(reader)
 
 
-dates, highs = [], []
+dates, highs, lows = [], [], []
 for row in reader:
     current_date = datetime.strptime(row[2], "%Y-%m-%d")
     high = int(row[4])
+    low = int(row[5])
     dates.append(current_date)
     highs.append(high)
-
+    lows.append(low)
 
 
 plt.style.use("seaborn-v0_8")
 fig, ax = plt.subplots()
 ax.plot(dates, highs, c="red")
+ax.plot(dates, lows, c="blue")
 
-ax.set_title("Daily high temperatures, July 2021", fontsize=24)
+ax.set_title("Daily high and low temperatures, 2021", fontsize=24)
 ax.set_xlabel("", fontsize=16)
 fig.autofmt_xdate()
 ax.set_ylabel("Temperature (F)", fontsize=16)
