d30cd17655bf8308c2d2ecc9bafe072de983eabf
Marvin P <MarvinPerez1503@outlook.com>
2026-05-05 09:35:28 -0400

    plot created

--- sitka_highs.py
+++ sitka_highs.py
@@ -1,5 +1,26 @@
 from pathlib import Path
 import csv 
+
+import matplotlib.pyplot as plt
+
 path = Path("sitka_weather_07-2021_simple.csv")
 lines = path.read_text(encoding="utf-8").splitlines()
-print(lines)
+
+reader = csv.reader(lines)
+header_row = next(reader)
+
+
+highs = []
+for row in reader:
+    high = int(row[5])
+    highs.append(high)
+
+plt.style.use("seaborn-v0_8")
+fig, ax = plt.subplots()
+ax.plot(highs, c="red")
+
+ax.set_title("Daily high temperatures, July 2021", fontsize=24)
+ax.set_xlabel("", fontsize=16)
+ax.set_ylabel("Temperature (F)", fontsize=16)
+ax.tick_params(labelsize=16)
+plt.show()
